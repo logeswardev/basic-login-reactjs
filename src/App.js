236d5f5659c8a1e25e0890f8b75752fbbe278c5b
lomadhavan <lomadhavan@ebay.com>
2023-06-29 14:39:24 -0400

    basic setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+
+  //Mock DataBase
+  const database = [
+    {
+      username : "user1",
+      password: "password1"
+    },
+    {
+      username : "user2",
+      password: "password2"
+    }
+  ];
+
+  const errors = {
+    uname: "errorUsername",
+    pass: "errorPassword"
+  }
+
+  const [errorMessage, setErrorMessage] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const renderErrorMessage = (name) =>
+      name === errorMessage.name && (
+          <div className="error">{errorMessage.message}</div>
+      );
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  let { uname, pass } = document.forms[0];
+
+  const userData = database.find((user) =>
+  user.username === uname.value)
+
+  if(userData){
+    if(userData.password !== pass.value){
+      setErrorMessage({
+        name: "pass",
+        message: errors.pass
+      });
+    }else {
+      setIsSubmitted(true);
+    }
+  }else {
+    setErrorMessage({
+      name: "username",
+      message: errors.uname
+    })
+  }
+
+  const renderForm = (
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label>Username </label>
+            <input type="text" name="uname" required />
+            {renderErrorMessage("uname")}
+          </div>
+          <div className="input-container">
+            <label>Password </label>
+            <input type="password" name="pass" required />
+            {renderErrorMessage("pass")}
+          </div>
+          <div className="button-container">
+            <input type="submit" />
+          </div>
+        </form>
+      </div>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="login-form">
+        <div className="title">Sign In</div>
+        {isSubmitted ? <div>User is successfully logged in</div> : renderForm}
+      </div>
     </div>
   );
 }
